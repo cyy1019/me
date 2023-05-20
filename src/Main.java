6ee2dfcd728182185cdeb8cc886802a958fffd89
cyy1019 <12210260@mail.edu.cn>
2023-05-20 22:43:29 +0800

    nothing

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-import controller.GameController;
-import model.Chessboard;
-import view.ChessGameFrame;
+import view.Menu;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            ChessGameFrame mainFrame = new ChessGameFrame(1100, 810);
-            GameController gameController = new GameController(mainFrame.getChessboardComponent(), new Chessboard());
-            mainFrame.setGameController(gameController);
-            gameController.setStatusLabel(mainFrame.getStatusLabel());
-            mainFrame.setVisible(true);
+            Menu mainMenu=new Menu(1100,810);
         });
     }
 }
